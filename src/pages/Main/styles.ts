27a0b8e8f524a8232a29ff1678e0a2f39ed5ee43
João Felipe N Santos <joaofnsantos@gmail.com>
2022-12-12 12:54:29 -0300

    :sparkles: feat: Adicionado pequena responsividade ao projeto

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -7,62 +7,77 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
 `
 
-export const ContainerCriptos = styled.div`
-width: 100%;
-height: 45rem;
-padding: 2rem 8rem;
-gap: 10rem;
-
-display: flex;
-align-items: center;
-justify-content: space-evenly;
-
-
+export const ContainerCriptos = styled.main`
+min-width: 100%;
+min-height: 100%;
 margin-top: 2rem;
-border-radius: 3rem;
+border-radius: 2rem;
 background:  rgb(225,228,232);
 
-.banner-text {
+display: flex;
+flex-direction: column;
+align-items: center;
+
+@media (min-width: 500px) {
   display: flex;
-  width: 45rem;
-  padding: 1rem;
-  
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+@media (min-width: 1000px) {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.banner-text {
+  @media (min-width: 350px) {
+      width: 40rem;
+      padding: 3.5rem;
+      display: flex;
+      text-align: center;
+     
+    }
+
   p{
-    font-size: 3rem;
-    font-weight: 900;
+    font-size: 2.5rem;
+    font-weight: 900;  
   }
 
   .banner-text-cripto {
     color: rgb( 231,150,9);
   }
+
 }
 `
 export const CriptoInfos = styled.div`
-width: 50rem;
+width: 40rem;
 height: 40rem;
 
 display: flex;
 align-items: center;
 justify-content: center;
 flex-wrap: wrap;
-gap: 2rem;
+gap: 1rem;
 
 
 img {
-  width: 7rem;
+  width: 5rem;
 }
 
 .cripto-details {
-  width: 15rem;
+  width: 14rem;
   padding: 1rem;
   border-radius: 1.5rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   gap: 0.7rem;
 
   box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
